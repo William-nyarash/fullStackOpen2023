5538af908608b6e46323882a0eeb98daee5d87f0
william-Nyarash <owetywilliamnyaranga@gmail.com>
2025-02-15 04:08:07 +0300

    final render of section a

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -10,6 +10,10 @@ const App = () => {
   const [password, setPassword] = useState('') 
   const [user, setUser] = useState(null)
   const [errorMessage, setErrorMessage] = useState('')
+  const [title,setTitle] =useState(' ')
+  const[author, setAuthor] = useState('')
+  const [url,setUrl] = useState('')
+  const [successMessage,setSuccessMessage] = useState(null)
 
 
   useEffect(() => {
@@ -60,16 +64,39 @@ const App = () => {
       console.log({ message: "Unable to clear record", error: err });
     }
   };
-  
+  useEffect(() => {
+    if (successMessage) {
+      const timer = setTimeout(() => {
+        setSuccessMessage(null);
+      }, 5000);
+      return () => clearTimeout(timer); 
+    }
+  }, [successMessage]);
+  const handleUserCreation = async (event) => {
+    event.preventDefault();
+    try {
+      const newBlog = await blogService.create({ title, author, url });
+      setBlogs(blogs.concat(newBlog)); 
+      setTitle('');
+      setAuthor('');
+      setUrl('');
+      setSuccessMessage(`a new ${newBlog.title} by ${newBlog.author} added`)
+
+    } catch (error) {
+      setErrorMessage('Error creating blog')
+    }
+  };
 
   return (
     <div>
       <h2>Blogs</h2>
 
-      {errorMessage && <p style={{color: 'red'}}>{errorMessage}</p>}
+      {errorMessage && <p style={{  borderRadius:'12px' ,  color:'red ', border: '2px solid red',padding:'12px'}}>{errorMessage}</p>}
+      {successMessage && <p style={{ borderRadius:'12px' ,  color:'green ',border: '4px solid green',padding:'12px' }}>{successMessage}</p>}
 
       {!user ? (
         <form onSubmit={handleLogin}>
+          <h2>log in to application</h2>
           <div>
             Username
             <input 
@@ -90,8 +117,26 @@ const App = () => {
         </form>
       ) : (
         <div>
-          <h2>Welcome, {user.username} <button type="button" onClick={Logout}>Logout</button>
+          <h2>{user.name}  logged in<button type="button" onClick={Logout}>Logout</button>
           </h2>
+          <div>
+            <h2>create new</h2>
+            <form onSubmit={handleUserCreation}>
+              <div>
+                title :
+                <input type="text" value={title} name='title'  onChange={({target})=>setTitle(target.value)}/>
+              </div>
+              <div>
+                author :
+                <input type="text" value={author} name='author' onChange={({target})=>setAuthor(target.value)} />
+              </div>
+              <div>
+                url :
+                <input type="text" value={url} name='url' onChange={({target})=>setUrl(target.value)} />
+              </div>
+              <button type="submit">create</button>
+            </form>
+          </div>
           {blogs.map(blog => 
             <Blog key={blog.id} blog={blog} />
           )}

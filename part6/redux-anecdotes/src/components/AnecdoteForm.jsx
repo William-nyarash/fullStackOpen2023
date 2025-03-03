79a97ab41fbc06072e9eac4b0602750ffe016739
william-Nyarash <owetywilliamnyaranga@gmail.com>
2025-03-04 00:33:00 +0300

    refactor the component for creating the anecdote

---
+++ part6/redux-anecdotes/src/components/AnecdoteForm.jsx
@@ -0,0 +1,26 @@
+import { useDispatch,useSelector } from "react-redux";
+import { addAnecdote } from "../reducers/anecdoteReducer";
+
+const AnecdoteForm =()=>{
+  const anecdotes = useSelector(state => state)
+  const dispatch = useDispatch()
+
+  const createAnecdote = (event) =>{
+    event.preventDefault()
+    const anecdote = event.target.anecdote.value
+    if(anecdote){
+      dispatch(addAnecdote(anecdote))
+      event.target.anecdote.value = ' '
+    }
+  }
+  return(
+    <div>
+      <h2>Create new</h2>
+      <form onSubmit={createAnecdote}>
+        <div><input type="text" name="anecdote" /></div>
+        <button type="submit" >create</button>
+      </form>
+    </div>
+  )
+}
+export default AnecdoteForm

fb69965bfb2e78dee2e0075c93f92ce426d591e2
william-Nyarash <owetywilliamnyaranga@gmail.com>
2025-03-04 00:00:27 +0300

    initial commit of anecdote app using redux

---
+++ part6/redux-anecdotes/src/App.jsx
@@ -0,0 +1,43 @@
+import { useSelector, useDispatch } from 'react-redux'
+import { handleVote, addAnecdote } from './reducers/anecdoteReducer'
+const App = () => {
+  const anecdotes = useSelector(state => state)
+  const dispatch = useDispatch()
+
+  const vote = (id) => {
+    dispatch(handleVote(id))
+    console.log("id is ",id)
+  }
+  const createAnecdote =(event) =>{
+    event.preventDefault()
+    const anecdote = event.target.anecdote.value.trim()
+    if(anecdote){
+      dispatch(addAnecdote(anecdote))
+      event.target.anecdote.value = ' '
+    }
+  }
+
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      {anecdotes.map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      )}
+      <h2>create new</h2>
+      <form onSubmit={createAnecdote}>
+        <div><input name='anecdote' /></div>
+        <button type='submit'>create</button>
+      </form>
+    </div>
+  )
+}
+
+export default App

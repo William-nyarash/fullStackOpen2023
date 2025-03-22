f4642d7ee44b7168580ca15222b8a6e09061b1c7
william-Nyarash <owetywilliamnyaranga@gmail.com>
2025-03-22 20:17:33 +0300

    add the missing button and its functionality

--- part6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -1,5 +1,6 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { createSelector } from "@reduxjs/toolkit";
+import { vote } from "../reducers/anecdoteReducer";
 
 const selectAnecdotes = state => state.anecdotes;
 const selectFilter = state => state.filter;
@@ -11,13 +12,19 @@ const selectFilteredAnecdotes = createSelector(
   }
 );
 const AnecdoteList = () => {
+  const dispatch = useDispatch()
   const anecdotes = useSelector(selectFilteredAnecdotes); 
+  
+  const handleVote =(id)=>{
+    dispatch(vote(id))
+  }
   return (
     <div>
       {anecdotes.map(anecdote => (
         <div key={anecdote.id}>
           <p>{anecdote.content}</p>
           <p>Votes: {anecdote.votes}</p>
+          <button onClick={()=>handleVote(anecdote.id)}>vote</button>
         </div>
       ))}
     </div>

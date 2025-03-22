60c32efeab651b641f7f97a3a477c6ed449e4ab5
william-Nyarash <owetywilliamnyaranga@gmail.com>
2025-03-22 19:55:47 +0300

    modify the anecdote list to use create selector function from redux/tookit

--- part6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -3,7 +3,6 @@ import { createSelector } from "@reduxjs/toolkit";
 
 const selectAnecdotes = state => state.anecdotes;
 const selectFilter = state => state.filter;
-
 const selectFilteredAnecdotes = createSelector(
   [selectAnecdotes, selectFilter],
   (anecdotes, filter) => {

4376cf829c5c1744f1206aa239001066565b599d
william-Nyarash <owetywilliamnyaranga@gmail.com>
2025-03-22 19:56:59 +0300

    impliment using configure store instead of create store

--- part6/redux-anecdotes/src/main.jsx
+++ part6/redux-anecdotes/src/main.jsx
@@ -1,9 +1,9 @@
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import App from './App'
-import filterReducer from './reducers/filterReducer'
 import { configureStore } from '@reduxjs/toolkit'
 import anecdoteReducer from './reducers/anecdoteReducer'
+import filterReducer from './reducers/filterReducer'
 
 const store  = configureStore({
   reducer:{

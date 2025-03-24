102cacb6363e3d93372746898142b5f9ec4ec937
william-Nyarash <owetywilliamnyaranga@gmail.com>
2025-03-25 00:42:50 +0300

    modify to be fetching data from our db

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -1,9 +1,20 @@
+import { useEffect } from 'react'
 import AnecdoteList from './components/AnecdoteList'
 import AnecdoteForm from './components/AnecdoteForm'
 import Filter from './components/FilterAnecdotes'
 import Notification from'./components/Notification'
-const App = () => {
+import { useDispatch } from 'react-redux'
+import { setAnecdotes } from './reducers/anecdoteReducer'
+import anecdotes from './services/anecdotes'
 
+const App = () => {
+  const dispatch = useDispatch()
+
+  useEffect( ()=>{
+    anecdotes.getAll().then( anecdote =>
+      dispatch(setAnecdotes(anecdote))
+    )
+  }, [ ])
   return (
     <div>
       <h2>Anecdotes</h2>

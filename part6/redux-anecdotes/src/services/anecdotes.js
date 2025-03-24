87d30fca2ad76250a9d5b4d4c65a8fc2f7db47ae
william-Nyarash <owetywilliamnyaranga@gmail.com>
2025-03-25 00:46:18 +0300

    add the function to create new anecdote and create an export for the same

--- part6/redux-anecdotes/src/services/anecdotes.js
+++ part6/redux-anecdotes/src/services/anecdotes.js
@@ -6,6 +6,10 @@ const getAll = async ()=> {
  const response = await axios.get(baseUrl)
  return response.data
 }
-
-export default { getAll }
+const createNew = async ({ content }) => {
+    const body =  content 
+    const respone = await axios.post(baseUrl, body)
+    return respone.data
+} 
+export default { getAll,createNew }
 

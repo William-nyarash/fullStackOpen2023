fd9edf9a0a17cca5b46c4f1fe13a70abc06ec274
william-Nyarash <owetywilliamnyaranga@gmail.com>
2025-03-25 00:43:56 +0300

    ensure that anecdote being created is saved on the database

--- part6/redux-anecdotes/src/components/AnecdoteForm.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,15 +1,18 @@
 import { useDispatch } from "react-redux";
 import { addAnecdote } from "../reducers/anecdoteReducer";
+import anecdotes from "../services/anecdotes";
 
 const AnecdoteForm =()=>{
   const dispatch = useDispatch()
 
-  const createAnecdote = (event) =>{
+  const createAnecdote = async (event) =>{
     event.preventDefault()
     const anecdote = event.target.anecdote.value
     if(anecdote){
-        dispatch(addAnecdote(anecdote))
+      const content = event.target.note.value
       event.target.anecdote.value = ' '
+      const newAnecdote = await anecdotes.createNew(content)
+      dispatch(addAnecdote(newAnecdote))
     }
   }
   return(

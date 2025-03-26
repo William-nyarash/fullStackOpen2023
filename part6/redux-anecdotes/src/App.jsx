3223e5736bfa8e66d74d64b6677367268929d029
william-Nyarash <owetywilliamnyaranga@gmail.com>
2025-03-27 00:56:50 +0300

    add notification component after fixing its functionality

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -1,29 +1,25 @@
-import { useEffect } from 'react'
-import AnecdoteList from './components/AnecdoteList'
-import AnecdoteForm from './components/AnecdoteForm'
-import Filter from './components/FilterAnecdotes'
-import Notification from'./components/Notification'
-import { useDispatch } from 'react-redux'
-import { setAnecdotes } from './reducers/anecdoteReducer'
-import anecdotes from './services/anecdotes'
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { initializeAnecdotes } from './reducers/anecdoteReducer';
+import AnecdoteList from './components/AnecdoteList';
+import AnecdoteForm from './components/AnecdoteForm';
+import Notification from './components/Notification';
 
 const App = () => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-  useEffect( ()=>{
-    anecdotes.getAll().then( anecdote =>
-      dispatch(setAnecdotes(anecdote))
-    )
-  }, [ ])
+  useEffect(() => {
+    dispatch(initializeAnecdotes());
+  }, [dispatch]);
+
   return (
     <div>
-      <h2>Anecdotes</h2>
-      <Filter />
+      <h1>Anecdotes</h1>
       <Notification />
-      <AnecdoteList />
       <AnecdoteForm />
+      <AnecdoteList />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;

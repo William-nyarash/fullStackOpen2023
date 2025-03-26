335f74543995bb20ca86ac9683505cad4fbd0ba7
william-Nyarash <owetywilliamnyaranga@gmail.com>
2025-03-27 00:54:07 +0300

    modify the service file

--- part6/redux-anecdotes/src/services/anecdotes.js
+++ part6/redux-anecdotes/src/services/anecdotes.js
@@ -2,15 +2,21 @@ import axios from 'axios'
 
 const baseUrl = 'http://localhost:3001/anecdotes'
 
-const getAll = async ()=> {
- const response = await axios.get(baseUrl)
- return response.data
+const getAll = async () => {
+  const response = await axios.get(baseUrl)
+  return response.data
 }
 
 const createNew = async (content) => {
-    const newAnecdote = { content, votes: 0 };
-    const response = await axios.post(baseUrl, newAnecdote);
-    return response.data;
-  };
-export default { getAll,createNew }
+  const newAnecdote = { content, votes: 0 }
+  const response = await axios.post(baseUrl, newAnecdote)
+  return response.data
+}
 
+const update = async (id, updatedAnecdote) => { 
+  const response = await axios.put(`${baseUrl}/${id}`, updatedAnecdote)
+  return response.data
+}
+
+const anecdoteService = { getAll, createNew, update }
+export default anecdoteService

fa68d830dc155fe353ec856c18e249b351fcae51
william-Nyarash <owetywilliamnyaranga@gmail.com>
2025-03-31 10:06:48 +0300

    add voting component

--- part6/query-anecdotes/src/App.jsx
+++ part6/query-anecdotes/src/App.jsx
@@ -1,19 +1,17 @@
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
-import { getAnecdote } from './request'
+import Vote from './components/Vote'
+import { getAnecdote, voteAnecdote } from './request'
 
 const App = () => {
   const queryClient = useQueryClient()
 
-  const { data: anecdotes, isLoading, isError, error } = useQuery({
+  const { data: anecdotes, isLoading } = useQuery({
     queryKey: ['anecdotes'],
     queryFn: getAnecdote,
     retry: 1, 
   })
-  const handleVote = (anecdote) => {
-    console.log('vote', anecdote)
-  }
 
   if (isLoading) {
     return <div>Loading anecdotes...</div>
@@ -27,11 +25,7 @@ const App = () => {
 
       {anecdotes.map((anecdote) => (
         <div key={anecdote.id}>
-          <div>{anecdote.content}</div>
-          <div>
-            has {anecdote.votes} votes
-            <button onClick={() => handleVote(anecdote)}>Vote</button>
-          </div>
+            <Vote anecdote={anecdote}/>
         </div>
       ))}
     </div>

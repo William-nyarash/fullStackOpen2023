b9a4830932222918cff60dd77b9aabc99ee2ac8c
william-Nyarash <owetywilliamnyaranga@gmail.com>
2025-03-31 10:07:46 +0300

    create voting component and ensure it works

---
+++ part6/query-anecdotes/src/components/Vote.jsx
@@ -0,0 +1,29 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { voteAnecdote } from "../request";
+
+// eslint-disable-next-line react/prop-types
+const Vote = ({anecdote}) => {
+  
+    const queryClient = useQueryClient()
+    const anecdoteToVoteFor = anecdote
+    const voteMutation= useMutation({
+        mutationFn: voteAnecdote,
+        onSuccess: () =>{
+            queryClient.invalidateQueries(['anecdotes'])
+        }
+    })
+    const handleVote = () => {
+        const updatedVotes = anecdoteToVoteFor.votes + 1
+        voteMutation.mutate({
+            ...anecdoteToVoteFor, votes: updatedVotes
+        })
+    }
+    return(
+        <div>
+            <div>{anecdoteToVoteFor.content}</div>
+            <div>has {anecdoteToVoteFor.votes} {" "} {anecdoteToVoteFor.votes === 1 ? 'vote' : 'votes'}</div>
+        <button onClick={handleVote}>vote</button>
+        </div>
+    )
+}
+export default Vote

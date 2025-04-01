43393ccd5d963e841dbbf4dd63da347e7a5538b9
william-Nyarash <owetywilliamnyaranga@gmail.com>
2025-04-01 23:20:43 +0300

    modify the component

--- part6/query-anecdotes/src/components/Notification.jsx
+++ part6/query-anecdotes/src/components/Notification.jsx
@@ -1,18 +1,38 @@
+"use client"
+
+import { useEffect } from "react"
+import { useNotification } from "./NotificationContext"
+
 const Notification = () => {
+  const { state, dispatch } = useNotification()
+
   const style = {
-    border: 'solid',
+    border: "solid",
     padding: 10,
     borderWidth: 1,
-    marginBottom: 5
+    marginBottom: 5,
   }
-  
-  if (true) return null
 
-  return (
-    <div style={style}>
-      
-    </div>
+  useEffect(() => {
+    let timer = null
+
+    if (state.isVisible) {
+      timer = setTimeout(() => {
+        dispatch({ type: "clear_notification" })
+      }, 5000)
+    }
+
+    return () => {
+      if (timer) clearTimeout(timer)
+    }
+  }, [state.isVisible, dispatch])
+
+  if (!state.isVisible) return null
+
+  return( 
+  <div style={style}>{state.message}</div>
   )
 }
 
 export default Notification
+

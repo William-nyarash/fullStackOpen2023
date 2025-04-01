4c40ae4557fc7a478aa75aa49d0f77734fca2d26
william-Nyarash <owetywilliamnyaranga@gmail.com>
2025-04-01 23:21:41 +0300

    modify the files

--- part6/query-anecdotes/src/components/AnecdoteForm.jsx
+++ part6/query-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,8 +1,10 @@
 import { useMutation,  useQueryClient } from '@tanstack/react-query'
 import { createAnecdote } from '../request'
+import { useNotification } from './NotificationContext'
 
 const AnecdoteForm = () => {
   const queryClient = useQueryClient()
+  const dispatch = useNotification
 
   const newAnecdoteMutation = useMutation({
     mutationFn: createAnecdote,
@@ -18,6 +20,7 @@ const AnecdoteForm = () => {
     
     if (anecdote && anecdote.length >= 5) {
       newAnecdoteMutation.mutate({ content: anecdote, votes: 0 })
+      dispatch({ type: 'SET_NOTIFICATION', payload: `Anecdote "${anecdote}" created!` });
     } else {
       alert("Anecdote content must be at least 5 characters long")
     }

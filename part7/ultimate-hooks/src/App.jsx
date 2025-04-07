d0ef7bb0e3baf08595a3e42556be0cd670566ad0
william-Nyarash <owetywilliamnyaranga@gmail.com>
2025-04-07 13:50:22 +0300

    modify the app to remove the rendering error

--- part7/ultimate-hooks/src/App.jsx
+++ part7/ultimate-hooks/src/App.jsx
@@ -1,38 +1,18 @@
-import { useState, useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import useResource from './hook'
+const useField =(type)=>{
+  const [value, setValue ] = useState("")
 
-const useField = (type) => {
-  const [value, setValue] = useState('')
-
-  const onChange = (event) => {
+  const onChange = (event)=> {
     setValue(event.target.value)
   }
 
-  return {
+  return{
     type,
-    value,
-    onChange
+    onChange, 
+    value
   }
 }
-
-// const useResource = (baseUrl) => {
-//   const [resources, setResources] = useState([])
-
-//   // ...
-
-//   const create = (resource) => {
-//     // ...
-//   }
-
-//   const service = {
-//     create
-//   }
-
-//   return [
-//     resources, service
-//   ]
-// }
-
 const App = () => {
   const content = useField('text')
   const name = useField('text')
@@ -41,29 +21,40 @@ const App = () => {
   const notes = useResource('http://localhost:3005/notes')
   const persons = useResource('http://localhost:3005/persons')
 
+  useEffect(()=>{
+    notes.getAll()
+    persons.getAll()
+  },[])
+  // useEffect(() => {
+  //       if (notes.resource && notes.resource.length > 0) {
+  //     content.setValue(notes.resource[0].content)
+  //   }
+
+  //   if (persons.resource && persons.resource.length > 0) {
+  //     name.setValue(persons.resource[0].name) 
+  //     number.setValue(persons.resource[0].number) 
+  //   }
+  // }, [notes.resource, persons.resource]) 
+
   const handleNoteSubmit = (event) => {
     event.preventDefault() 
     notes.create({ content: content.value })
     
   }
-  useEffect(()=>{
-    //  notes.getAll()
-     
-  }, [notes])
+
  
   const handlePersonSubmit = (event) => {
     event.preventDefault()
     persons.create({ name: name.value, number: number.value})
   }
-
   return (
     <div>
       <h2>notes</h2>
-      <form onSubmit={handleNoteSubmit}>
+      <form onSubmit={handleNoteSubmit} >
         <input {...content} />
         <button>create</button>
       </form>
-      {/* {notes.map(n => <p key={n.id}>{n.content}</p>)} */}
+      {notes.resource && notes.resource.map(n => <p key={n.id}>{n.content}</p>)}
 
       <h2>persons</h2>
       <form onSubmit={handlePersonSubmit}>
@@ -71,7 +62,7 @@ const App = () => {
         number <input {...number} />
         <button>create</button>
       </form>
-      {/* {persons.map(n => <p key={n.id}>{n.name} {n.number}</p>)} */}
+      {persons.resource && persons.resource.map(n => <p key={n.id}>{n.name} {n.number}</p>)}
     </div>
   )
 }

81b04c1f51c75417f3b2dec6bf114f68b27b92b1
william-Nyarash <owetywilliamnyaranga@gmail.com>
2025-05-24 15:23:33 +0300

    impliment fetching input from the commandline interface

--- part9/exercise9.1-9.7/bmiCalculator.ts
+++ part9/exercise9.1-9.7/bmiCalculator.ts
@@ -1,26 +1,51 @@
 type Prognosis = 'normal range' | 'over weight' | 'under weight' | 'obese' | 'invalid input';
 
-const calculateBmi = (height: number, weight: number): Prognosis => {
-    if (height <= 0) {
-        console.log(`Invalid input`);
-        return 'invalid input';
-    }
-
-    const standardheight = height / 100;
-    const result = weight / (standardheight * standardheight);
-    let BMI = result;
-
-    if (BMI < 18.5) {
-        console.log( "normal")
-    } else if (BMI >= 18.5 && BMI <= 24.9) {
-         console.log('normal range');
-    } else if (BMI >= 25 && BMI <= 29.9) {
-        console.log('over weight');
-    } else if (BMI >= 30) {
-        return 'obese';
-    } else {
-        console.log(`Invalid input`);
-        return 'invalid input';
-    }
+interface bmivalues {
+    value1: number
+    value2:number
 }
-calculateBmi(180, 74);
+const dataFromCli = ( args: string [] ):bmivalues=> {
+    if(args.length < 4) throw new Error('less arguments than expected') 
+    if(args.length > 4) throw new Error('too much arguments than expected') 
+
+        if (isNaN(Number(args[2])) || isNaN(Number(args[3]))) {
+            throw new Error('Provided values were not number!');
+        } else {
+            return {
+                value1: Number(args[2]),
+                value2: Number(args[3])
+            };
+        }
+    }
+const calculateBmi = (height: number, weight: number): Prognosis => {
+    
+    if (height <= 0 || weight <= 0 || isNaN(height) || isNaN(weight)) {
+        return 'invalid input';
+      }
+    
+      const heightInMeters = height / 100;
+      const bmi = weight / (heightInMeters * heightInMeters);
+    
+      if (bmi < 18.5) {
+        return 'under weight';
+      } else if (bmi <= 24.9) {
+        return 'normal range';
+      } else if (bmi <= 29.9) {
+        return 'over weight';
+      } else if (bmi >= 30) {
+        return 'obese';
+      } else {
+        return 'invalid input';
+      }
+}
+
+try {
+    const {value1, value2} = dataFromCli(process.argv);
+    console.log(`Prognosis:${calculateBmi(value1,value2)}`);
+}catch (error: unknown) {
+ let errorMessage= 'An error occured'
+ if(error instanceof Error) {
+    errorMessage +='Error: ' + error.message;
+ }
+ console.log(errorMessage);
+}
--- part9/exercise9.1-9.7/exerciseCalculator.ts
+++ part9/exercise9.1-9.7/exerciseCalculator.ts
@@ -7,10 +7,22 @@ interface sessions {
     target:number
     average:number
 }
-var exercise: number[] = [];
 
-const calculateExercises = ( dailyExerciseHours: number[], target: number ) => {
+const parsedargs =(args: string []):{target: number ,dailyExerciseHours: number[]}=> {
+    if( args.length < 4 ) throw new Error("Too few arguments");
 
+    const target = Number(args[2]);
+    if (isNaN(target)) throw new Error('Provided target value is not a number!');
+    const dailyExerciseHours= args.slice(3).map( arg => {
+        const value = Number(arg);
+        if(isNaN(value)){
+            throw new Error("Provided values should be numbers")
+        }
+        return  value
+    });
+    return { target, dailyExerciseHours}
+}
+const calculateExercises = ( dailyExerciseHours: number[], target: number ): sessions=> {
     const periodLength =  dailyExerciseHours.length ;
     const trainingDays = dailyExerciseHours.filter(h => h > 0 ).length;
     const sumOfHours  = dailyExerciseHours.reduce((sum , h) => sum += h , 0);
@@ -18,7 +30,6 @@ const calculateExercises = ( dailyExerciseHours: number[], target: number ) => {
 
     let rating: number;
     let ratingDescription: string;
-     console.log(`the average is : ${average}`);
      
      if (average >= target) {
         rating = 3;
@@ -44,4 +55,13 @@ const calculateExercises = ( dailyExerciseHours: number[], target: number ) => {
       }
 
 }
-console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2));
+try{
+    const {target,dailyExerciseHours}= parsedargs(process.argv);
+    console.log('the values are :', calculateExercises(dailyExerciseHours, target));
+} catch (error: unknown) {
+    let errorMessage = 'An error occurred: ';
+    if (error instanceof Error) {
+        errorMessage += error.message;
+    }
+    console.log(errorMessage);
+}

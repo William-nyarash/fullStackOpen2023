e672f53a13ceebfaeb773fefed2ed27ec8f0f695
william-Nyarash <owetywilliamnyaranga@gmail.com>
2025-06-23 20:47:17 +0300

    impliment body mass index calculator

--- part9/exercise9.1-9.7/index.ts
+++ part9/exercise9.1-9.7/index.ts
@@ -1,10 +1,26 @@
 import express from 'express';
+import { calculateBmi } from './bmiCalculator';
 
 const app = express();
 
 app.get('/hello', (_req, res)=>{
     res.send('Hello Full Stack!');
 })
+
+app.get('/bmi', (req, res) => {
+    const weight = Number(req.query.weight);
+    const height = Number(req.query.height);
+
+    if(isNaN(weight) || isNaN(height)) res.status(400).send({error: "malformatted parameters"})
+  
+    const prognosis = calculateBmi(height, weight);
+  
+    res.send({
+      weight,
+      height,
+      bmiResults: prognosis
+    });
+  });
 
 const port = 3002;
 

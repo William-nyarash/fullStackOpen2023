d5e5a5c38895fbbdf7986e943098282a79a1b345
william-Nyarash <owetywilliamnyaranga@gmail.com>
2025-06-30 17:34:31 +0300

    disable auto query on window focus

--- part6/query-anecdotes/src/App.jsx
+++ part6/query-anecdotes/src/App.jsx
@@ -2,7 +2,7 @@ import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
 import Vote from './components/Vote'
-import { getAnecdote, voteAnecdote } from './request'
+import { getAnecdote} from './request'
 import { NotificationProvider } from './components/NotificationContext'
 
 
@@ -12,26 +12,25 @@ const App = () => {
   const { data: anecdotes, isLoading } = useQuery({
     queryKey: ['anecdotes'],
     queryFn: getAnecdote,
+    refetchOnWindowFocus:false,
     retry: 1, 
   })
 
   if (isLoading) {
     return <div>Loading anecdotes...</div>
   }
+
   return (
     <NotificationProvider>
       <h3>Anecdote App</h3>
-
       <Notification />
       <AnecdoteForm />
-
       {anecdotes.map((anecdote) => (
         <div key={anecdote.id}>
             <Vote anecdote={anecdote}/>
         </div>
       ))}
     </NotificationProvider>
-  )
-}
+  )}
 
 export default App

66d61ec552179710c5fdb8b7c97e257f648c32c9
William-nyarash <owetywilliamnyaranga@gmail.com>
2025-07-14 19:26:39 +0300

    refactor the query definitions

--- part8/graphQltest/graphQlClient/src/main.jsx
+++ part8/graphQltest/graphQlClient/src/main.jsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import {
+   ApolloClient,
+   ApolloProvider,
+   InMemoryCache} from '@apollo/client'
+import { query } from './queries.js'
+const client = new ApolloClient({
+  uri: 'http://localhost:4000',
+  cache: new InMemoryCache(),
+})
+
+client.query({ query })
+  .then((response) => {
+    console.log(response.data)
+  })
+ReactDOM.createRoot(document.getElementById('root')).render(
+<ApolloProvider client={client}>
+  <App />
+</ApolloProvider>
 )
